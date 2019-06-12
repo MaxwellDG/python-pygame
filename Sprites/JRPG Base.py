173a0766bdde77b96db4aa4d857aba977aeea93f
Mgreeeen <mgreeeen@live.com>
2019-06-11 20:14:33 -0400

    Adding all the sprite files

---
+++ Sprites/JRPG Base.py
@@ -0,0 +1,17 @@
+#check out pickle for saving and loading
+
+# To do list:
+# make the toon flash red if shot (probably need to draw this yourself. just do a shitty red border around one of each
+    # direction
+# make some sliding option and use the math module and physics programming
+# re-do the layout of the ninja room
+
+
+
+
+
+
+
+
+
+
---
+++ Sprites/Sprite_Animation.py
@@ -0,0 +1,144 @@
+import pygame
+
+
+# FIND SOME BETTER BACKGROUNDS FROM REAL RPGS. THINK OF POKEMON FOR EXAMPLE. OR GOLDEN SUN. THOSE KIND OF ROOMS.
+
+# Jeremy
+
+jer_selector = [pygame.image.load("../Sprites/Jer.Down.1.128.png"), pygame.image.load("../Sprites/Jer.Down.2.128.png"),
+                pygame.image.load("../Sprites/Jer.Down.3.128.png")]
+
+jer_left_sprite = [pygame.image.load("../Sprites/Jer.Left.1.png"), pygame.image.load("../Sprites/Jer.Left.2.png"),
+                   pygame.image.load("../Sprites/Jer.Left.3.png")]
+jer_right_sprite = [pygame.image.load("../Sprites/Jer.Right.1.png"), pygame.image.load("../Sprites/Jer.Right.2.png"),
+                    pygame.image.load("../Sprites/Jer.Right.3.png")]
+jer_up_sprite = [pygame.image.load("../Sprites/Jer.Up.1.png"), pygame.image.load("../Sprites/Jer.Up.2.png"),
+                 pygame.image.load("../Sprites/Jer.Up.3.png")]
+jer_down_sprite = [pygame.image.load("../Sprites/Jer.Down.1.png"), pygame.image.load("../Sprites/Jer.Down.2.png"),
+                   pygame.image.load("../Sprites/Jer.Down.3.png")]
+
+# Martha
+
+mar_selector = [pygame.image.load("../Sprites/Martha.Down.1.128.png"),
+                pygame.image.load("../Sprites/Martha.Down.2.128.png"),
+                pygame.image.load("../Sprites/Martha.Down.3.128.png")]
+
+mar_left_sprite = [pygame.image.load("../Sprites/Martha.Left.1.png"), pygame.image.load("../Sprites/Martha.Left.2.png"),
+                   pygame.image.load("../Sprites/Martha.Left.3.png")]
+mar_right_sprite = [pygame.image.load("../Sprites/Martha.Right.1.png"),
+                    pygame.image.load("../Sprites/Martha.Right.2.png"),
+                    pygame.image.load("../Sprites/Martha.Right.3.png")]
+mar_up_sprite = [pygame.image.load("../Sprites/Martha.Up.1.png"), pygame.image.load("../Sprites/Martha.Up.2.png"),
+                 pygame.image.load("../Sprites/Martha.Up.3.png")]
+mar_down_sprite = [pygame.image.load("../Sprites/Martha.Down.1.png"), pygame.image.load("../Sprites/Martha.Down.2.png"),
+                   pygame.image.load("../Sprites/Martha.Down.3.png")]
+
+# Sayain
+
+say_left_sprite = [pygame.image.load("../Sprites/say.Left.1.png"), pygame.image.load("../Sprites/say.Left.2.png"),
+                   pygame.image.load("../Sprites/say.Left.3.png")]
+say_right_sprite = [pygame.image.load("../Sprites/say.Right.1.png"), pygame.image.load("../Sprites/say.Right.2.png"),
+                    pygame.image.load("../Sprites/say.Right.3.png")]
+say_up_sprite = [pygame.image.load("../Sprites/say.Up.1.png"), pygame.image.load("../Sprites/say.Up.2.png"),
+                 pygame.image.load("../Sprites/say.Up.3.png")]
+say_down_sprite = [pygame.image.load("../Sprites/say.Down.1.png"), pygame.image.load("../Sprites/say.Down.2.png"),
+                   pygame.image.load("../Sprites/say.Down.3.png")]
+
+
+# Goblin(Left Room)
+
+gob_left_sprite = [pygame.image.load("../Sprites/gob.Left.1.png"), pygame.image.load("../Sprites/gob.Left.2.png"),
+                   pygame.image.load("../Sprites/gob.Left.3.png")]
+gob_right_sprite = [pygame.image.load("../Sprites/gob.Right.1.png"), pygame.image.load("../Sprites/gob.Right.2.png"),
+                    pygame.image.load("../Sprites/gob.Right.3.png")]
+gob_up_sprite = [pygame.image.load("../Sprites/gob.Up.1.png"), pygame.image.load("../Sprites/gob.Up.2.png"),
+                 pygame.image.load("../Sprites/gob.Up.3.png")]
+gob_down_sprite = [pygame.image.load("../Sprites/gob.Down.1.png"), pygame.image.load("../Sprites/gob.Down.2.png"),
+                   pygame.image.load("../Sprites/gob.Down.3.png")]
+gob_dead_sprite = pygame.image.load("../Sprites/gob.dead.png")
+
+# Mech(Top Room)
+
+mech_left_sprite = [pygame.image.load("../Sprites/mech.Left.1.png"), pygame.image.load("../Sprites/mech.Left.2.png"),
+                    pygame.image.load("../Sprites/mech.Left.3.png")]
+mech_right_sprite = [pygame.image.load("../Sprites/mech.Right.1.png"), pygame.image.load("../Sprites/mech.Right.2.png"),
+                     pygame.image.load("../Sprites/mech.Right.3.png")]
+mech_up_sprite = [pygame.image.load("../Sprites/mech.Up.1.png"), pygame.image.load("../Sprites/mech.Up.2.png"),
+                  pygame.image.load("../Sprites/mech.Up.3.png")]
+mech_down_sprite = [pygame.image.load("../Sprites/mech.Down.1.png"), pygame.image.load("../Sprites/mech.Down.2.png"),
+                    pygame.image.load("../Sprites/mech.Down.3.png")]
+mech_dead_sprite = [pygame.image.load("../Sprites/mech.dead.png"), pygame.image.load("../Sprites/mech.dead.2.png"),
+                    pygame.image.load("../Sprites/mech.dead.3.png"), pygame.image.load("../Sprites/mech.dead.4.png"),
+                    pygame.image.load("../Sprites/mech.dead.5.png"), pygame.image.load("../Sprites/mech.dead.6.png"),
+                    pygame.image.load("../Sprites/mech.dead.7.png")]
+
+# Ninja(Right Room)
+
+nin_left_sprite = [pygame.image.load("../Sprites/nin.Left.1.png"), pygame.image.load("../Sprites/nin.Left.2.png"),
+                   pygame.image.load("../Sprites/nin.Left.3.png")]
+nin_right_sprite = [pygame.image.load("../Sprites/nin.Right.1.png"), pygame.image.load("../Sprites/nin.Right.2.png"),
+                    pygame.image.load("../Sprites/nin.Right.3.png")]
+nin_up_sprite = [pygame.image.load("../Sprites/nin.Up.1.png"), pygame.image.load("../Sprites/nin.Up.2.png"),
+                 pygame.image.load("../Sprites/nin.Up.3.png")]
+nin_down_sprite = [pygame.image.load("../Sprites/nin.Down.1.png"), pygame.image.load("../Sprites/nin.Down.2.png"),
+                   pygame.image.load("../Sprites/nin.Down.3.png")]
+nin_dead_sprite = pygame.image.load("../Sprites/nin.dead.png")
+
+# Scenery
+
+random_tree = pygame.image.load("../Sprites/random_tree.png")
+random_bush = pygame.image.load("../Sprites/random_bush.png")
+treasure_open = pygame.image.load("../Sprites/treasure_open.png")
+treasure_closed = pygame.image.load("../Sprites/treasure_closed.png")
+random_rock_wall = pygame.image.load("../Sprites/random_rock_wall.png")
+random_rock_wall_vert = pygame.image.load("../Sprites/random_rock_wall_vert.png")
+random_pillar = pygame.image.load("../Sprites/random_pillar.png")
+
+the_sign = pygame.image.load("../Sprites/info_sign.png")
+the_arrow = pygame.image.load("../Sprites/arrow.png")
+
+garb = pygame.image.load("../Sprites/garb.png")
+pewpew = pygame.image.load("../Sprites/pewpew.png")
+computer = pygame.image.load("../Sprites/computer.png")
+
+# Other
+
+blue_beam_right = pygame.image.load("../Sprites/blue_beam_right.png")
+blue_beam_left = pygame.image.load("../Sprites/blue_beam_left.png")
+blue_beam_up = pygame.image.load("../Sprites/blue_beam_up.png")
+blue_beam_down = pygame.image.load("../Sprites/blue_beam_down.png")
+
+purp_beam_right = pygame.image.load("../Sprites/purp_beam_right.png")
+purp_beam_left = pygame.image.load("../Sprites/purp_beam_left.png")
+purp_beam_up = pygame.image.load("../Sprites/purp_beam_up.png")
+purp_beam_down = pygame.image.load("../Sprites/purp_beam_down.png")
+
+stairwell = pygame.image.load("../Sprites/stairwell.png")
+
+healthbar = pygame.image.load("../Sprites/healthbar.png")
+lost_health = pygame.image.load("../Sprites/lost_health.png")
+
+# backgrounds
+
+background_start = pygame.image.load("../Sprites/Home.Background.png")
+background_left = pygame.image.load("../Sprites/left_room_background.png")
+background_right = pygame.image.load("../Sprites/right_room_background.png")
+background_top = pygame.image.load("../Sprites/top_room_background.png")
+
+# fireworks
+
+blueshot = [pygame.image.load("../Sprites/blueshot1.png"), pygame.image.load("../Sprites/blueshot2.png"),
+            pygame.image.load("../Sprites/blueshot3.png"), pygame.image.load("../Sprites/blueshot4.png"),
+            pygame.image.load("../Sprites/blueshot5.png"), pygame.image.load("../Sprites/blueshot6.png"),
+            pygame.image.load("../Sprites/blueshot7.png"), pygame.image.load("../Sprites/blueshot8.png")]
+
+
+violetshot = [pygame.image.load("../Sprites/violetshot1.png"), pygame.image.load("../Sprites/violetshot2.png"),
+              pygame.image.load("../Sprites/violetshot3.png"), pygame.image.load("../Sprites/violetshot4.png"),
+              pygame.image.load("../Sprites/violetshot5.png"), pygame.image.load("../Sprites/violetshot6.png"),
+              pygame.image.load("../Sprites/violetshot7.png"), pygame.image.load("../Sprites/violetshot8.png")]
+
+yellowshot = [pygame.image.load("../Sprites/yellowshot1.png"), pygame.image.load("../Sprites/yellowshot2.png"),
+              pygame.image.load("../Sprites/yellowshot3.png"), pygame.image.load("../Sprites/yellowshot4.png"),
+              pygame.image.load("../Sprites/yellowshot5.png"), pygame.image.load("../Sprites/yellowshot6.png"),
+              pygame.image.load("../Sprites/yellowshot7.png"), pygame.image.load("../Sprites/yellowshot8.png")]
